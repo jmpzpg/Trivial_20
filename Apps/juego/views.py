baf567dbbca01f288363ac03d2de4c5e5bd50a6b
jmpzpg <92719993+jmpzpg@users.noreply.github.com>
2022-05-11 01:20:10 +0200

    no está listo pero hay que entregarlo

--- Apps/juego/views.py
+++ Apps/juego/views.py
@@ -1,9 +1,14 @@
+from multiprocessing import context
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
-from .models import Pregunta, Respuesta, Curso, Docente
-#from models import Pregunta, Respuesta, Curso, Docente
+from .models import Pregunta, Respuesta 
+
 import random
 import pdb
+
+
+
 
 # Create your views here.
 
@@ -17,6 +22,7 @@ def inicio(request):
     return render(request, 'base.html', datos)
 
 def comenzar_partida(request):
+    
     #dic_respuestas_para_pregunta = respuestas_para_cada_pregunta()
     #dic_idPreg_y_dic_TxtPreg_y_lista_tuplas_idyTextResp = respuestas_para_cada_pregunta()
     dic_idPreg_y_dic_TxtPreg_y_lista_tuplas_idyTextResp_selecc_aleatoria = respuestas_para_cada_pregunta()
@@ -24,7 +30,7 @@ def comenzar_partida(request):
     listado_respuestas = Respuesta.objects.all()
     list_id_texto_preguntas = Pregunta.objects.values_list('id', 'texto')
     list_id_preguntas = Pregunta.objects.values_list('id', flat=True)
-    cojonudo = tomar_preguntas_aleatorias()
+    
     datos = {
         'titulo': 'Listado de respuestas',
         'preguntas': listado_preguntas,
@@ -32,10 +38,10 @@ def comenzar_partida(request):
         'preguntas_selecc_aleat_con_sus_respuestas':dic_idPreg_y_dic_TxtPreg_y_lista_tuplas_idyTextResp_selecc_aleatoria,
         'respuestas_a_pregunta': partida_pregunta(request, 2),
         'id_preguntas': list_id_preguntas,
-        'dic_p_r': preguntas_respuestas(),
-        'preg_alea': cojonudo
+        'dic_p_r': preguntas_respuestas()
+
     }
-    return render(request, 'partida10.html', datos)
+    return render(request, 'partida20.html', datos)
 
 def comenzar_partida_2(request):
     dic_respuestas_para_pregunta = respuestas_para_cada_pregunta()
@@ -152,30 +158,20 @@ def sacar_lista_preg_aleatorias_de_tablaPreguntas():
     return listado_todas_las_preg_barajadas
 
 def tomar_preguntas_aleatorias():
-
     #pdb.set_trace()
     # primero tomamos una lista de dicc con todos los ID de la tabla Preguntas
     l_dic_preguntas_ids = Pregunta.objects.all().values('id') #es una lista de dicc id:valor
-    # segundo creamos varialbe para guardar la lista de  esos ID
+    # segundo creamos variable para guardar la lista de  esos ID
     lista_id_preg = []
     for dic in l_dic_preguntas_ids:
         lista_id_preg.append(dic['id'])
     a = list(lista_id_preg)
-    # Ya tenemos la lista de ID de Preguntas. Ahora la barajamos
-    b = random.sample(lista_id_preg, 2)
-    # Ya tenemos la misma lista barajada.
+    # Ya tenemos la lista de ID de Preguntas. Ahora la barajamos y seleccionamos numero
+    b = random.sample(lista_id_preg, 10)
+    # Ya tenemos la misma lista con las preguntas seleccionadas y barajadas.
     # Ahora hay que reconstruir el dicc para tener los ID y sus textos
     list_tuplas_IdyTexto_de_preguntas_selecc_aleatoria = Pregunta.objects.filter(id__in = b).values_list('id', 'texto')
 
-
-
-    #if len(preguntas_ids) > 0:
-        #random_id = random.choice(preguntas_ids)['id']
-    
-    #random_id = random.choices(l_dic_preguntas_ids, k=4)
-    #preg_aleatorias = Pregunta.objects.filter(id__in = random_id)
-    
-    #for preg in preg_aleatorias:
     return list_tuplas_IdyTexto_de_preguntas_selecc_aleatoria
     #resp_de_preg_aleat = Respuesta.objects.filter(pregunta=id in random_id).values_list('id', 'texto')
                
@@ -185,88 +181,29 @@ def responder_pregunta(request, id):
     pass
 
 
-def seleccionar_respuesta(request, id_p, id_r):
-    pass
+def validar_respuesta(request, id_p, id_r):
+    num_resp_acertadas = request.session.get('num_resp_acertadas' , 0)
+    
+    a = Respuesta.objects.get(pregunta=id_p , correcta=1)
+    b = Respuesta.objects.filter(pregunta=id_p , correcta=1).values('id')
+
+    id_resp_correcta = b[0]['id']
+    if id_r == id_resp_correcta:
+        
+
+        request.session['num_resp_acertadas'] = num_resp_acertadas + 1
+
+    context = {
+        'num_resp_acertadas': num_resp_acertadas,
+    }
+    return render(request, 'ver_respuesta.html', context=context)
+    #dic_salida = { 'nra': contador}
+    #return render(request, 'partida20.html', dic_salida)
+
 
 
 # ===============================================================
 
-def home(request):
-    cursos_listados = Curso.objects.all()
-    # cursos_listados = Curso.objects.all()[:5]
-    # cursos_listados = Curso.objects.all()[4:9]
-    # cursos_listados = Curso.objects.all().order_by('nombre')
-    # cursos_listados = Curso.objects.all().order_by('-nombre')
-    # cursos_listados = Curso.objects.all().order_by('nombre', 'creditos')
-    # cursos_listados = Curso.objects.filter(nombre='Historia', creditos=5)
-    # cursos_listados = Curso.objects.filter(creditos__lte=4)
-    # cursos_listados = Curso.objects.filter(nombre__startswith='Q')
-    # cursos_listados = Curso.objects.filter(nombre__contains='g')
-
-    # return HttpResponse("<h1>Hola Mundo!</h1>")
-    data = {
-        'titulo': 'HOME - Gestión de Cursos',
-        'cursos': cursos_listados
-    }
-    # return render(request, "gestionCursos.html", {"cursos": cursos_listados})
-    return render(request, "gestionCursos.html", data)
-
-
-class CursoListView(ListView):
-    model = Curso
-    template_name = 'gestionCursos.html'
-
-    def get_queryset(self):
-        # return Curso.objects.filter(creditos__lte=4)
-        return Curso.objects.all().order_by('nombre')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['titulo'] = 'LV - Gestión de Cursos'
-        # print(context)
-        return context
-
-
-def registrar_curso(request):
-    nombre = request.POST['txtNombre']
-    creditos = request.POST['numCreditos']
-
-    curso = Curso.objects.create(nombre=nombre, creditos=creditos)
-    return redirect('/')
-
-
-def eliminar_curso(request, id):
-    curso = Curso.objects.get(id=id)
-    curso.delete()
-
-    return redirect('/')
-
-
-def edicion_curso(request, id):
-    curso = Curso.objects.get(id=id)
-    data = {
-        'titulo': 'Edición de Curso',
-        'curso': curso
-    }
-
-    return render(request, "edicionCurso.html", data)
-
-
-def editar_curso(request):
-    id = int(request.POST['id'])
-    nombre = request.POST['txtNombre']
-    creditos = request.POST['numCreditos']
-
-    curso = Curso.objects.get(id=id)
-    curso.nombre = nombre
-    curso.creditos = creditos
-    curso.save()
-
-    return redirect('/')
-
-
-def contacto(request):
-    return render(request, "contacto.html")
 
 
 
